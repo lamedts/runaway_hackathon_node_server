9271b866ff63a1fd7ffc18d689134b94361d1216
James <lo.jimmy1994@gmail.com>
2017-08-19 19:01:32 +0800

    Added semantic ui

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.env || 'development';
 const VIEWS_PATH = path.join(__dirname, 'views');
 const DIST_PATH = path.join(__dirname, '..', 'dist');
+const SEMANTIC_UI_PATH = path.join(__dirname, '..', 'semantic');
 
 app.set('port', PORT);
 app.set('views', VIEWS_PATH);
@@ -13,6 +14,7 @@ app.set('view engine', 'ejs');
 
 // For stylesheets & scripts files loading
 app.use(express.static(DIST_PATH));
+app.use(express.static(SEMANTIC_UI_PATH));
 
 app.get('/', function (req, res) {
   res.render('index');
